ccb17d7e087e010ae0ee15a993b47b6c9010b753
konditi1 <konditi1@gmail.com>
2024-01-23 23:48:11 +0300

    function that changes all topics of a school document based on the name

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+function that changes all topics of a school document based on the name
+"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    changes all topics of a school document based on the name
+    """
+    return mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
